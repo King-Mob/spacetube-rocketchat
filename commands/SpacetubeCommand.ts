e2675c53d135a4bdba85f65e212b0c32294eaf1e
KingMob <john@onethreethree.net>
2025-05-05 14:20:15 +0100

    put spaces back in

--- commands/SpacetubeCommand.ts
+++ commands/SpacetubeCommand.ts
@@ -2,65 +2,69 @@ import {
     IHttp,
     IModify,
     IRead,
-    ILogger
-} from '@rocket.chat/apps-engine/definition/accessors';
+    ILogger,
+} from "@rocket.chat/apps-engine/definition/accessors";
 import {
     ISlashCommand,
     SlashCommandContext,
-} from '@rocket.chat/apps-engine/definition/slashcommands';
+} from "@rocket.chat/apps-engine/definition/slashcommands";
 
 export async function forward(context) {
     fetch("https://spacetube.spacetu.be/rocketchat/event", {
         method: "POST",
         body: JSON.stringify({
-            ...context
+            ...context,
         }),
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
+            "Content-Type": "application/json",
+        },
+    });
 }
 
 async function register(url) {
     fetch("https://spacetube.spacetu.be/rocketchat/register", {
         method: "POST",
         body: JSON.stringify({
-            url
+            url,
         }),
         headers: {
-            "Content-Type": "application/json"
-        }
-    })
+            "Content-Type": "application/json",
+        },
+    });
 }
 
-async function sendMessage(context: SlashCommandContext, modify: IModify, message: string): Promise<void> {
+async function sendMessage(
+    context: SlashCommandContext,
+    modify: IModify,
+    message: string
+): Promise<void> {
     const messageStructure = modify.getCreator().startMessage();
     const sender = context.getSender();
     const room = context.getRoom();
 
-    messageStructure
-        .setSender(sender)
-        .setRoom(room)
-        .setText(message);
+    messageStructure.setSender(sender).setRoom(room).setText(message);
 
     await modify.getCreator().finish(messageStructure);
 }
 
 export class SpacetubeCommand implements ISlashCommand {
-    public command = 'spacetube';
-    public i18nParamsExample = '';
-    public i18nDescription = '';
+    public command = "spacetube";
+    public i18nParamsExample = "";
+    public i18nDescription = "";
     public providesPreview = false;
-    private readonly appLogger: ILogger
+    private readonly appLogger: ILogger;
 
     constructor(logger: ILogger) {
-        this.appLogger = logger
+        this.appLogger = logger;
     }
 
-    public async executor(context: SlashCommandContext, read: IRead, modify: IModify, http: IHttp): Promise<void> {
-
-
-        const params = context.getArguments().join("");
+    public async executor(
+        context: SlashCommandContext,
+        read: IRead,
+        modify: IModify,
+        http: IHttp
+    ): Promise<void> {
+        const params = context.getArguments().join(" ");
 
         // test for registration
         if (params.includes("c16853cf-c591-4453-b1dd-7def3e08cf02")) {
@@ -69,6 +73,6 @@ export class SpacetubeCommand implements ISlashCommand {
             forward(context);
         }
 
-        sendMessage(context, modify, "Spacetubing!");
+        sendMessage(context, modify, `/spacetube ${params}`);
     }
 }
